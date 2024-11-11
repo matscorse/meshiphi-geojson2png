ecdd54bf149e7723416e8796336dad40a9a8cf0c
matscorse <matsco@bas.ac.uk>
2024-11-11 15:16:13 +0000

    made conversion script into entrypoint

--- src/geojson2png/geojson2png.py
+++ src/geojson2png/geojson2png.py
@@ -13,19 +13,38 @@ import matplotlib.pyplot as plt
 
 _XSIZE = 40
 _YSIZE = 30
+_DPI = 180
 
-def convert():
-    df = gpd.read_file('amsr_southern_SDA.vessel.geojson')
+def check_input_filenames(input_filenames: list):
+    """
+    from the cli arguments create a list of which files
+    actually exist.
+    """
+    list_of_filenames_that_actually_exist = []
 
+    for a_filename in input_filenames:
+        if not os.path.isfile(a_filename):
+            print("Filename %s is not a file (or doesn't exist): Ignoring", a_filename)
+        else:
+            list_of_filenames_that_actually_exist.append(os.path.abspath(a_filename))
+            print("Including filename: %s", a_filename)
+
+    return list_of_filenames_that_actually_exist
+
+
+
+def convert_to_png(geojson_filepath: os.path, xsize, ysize, dpi):
+
+    df = gpd.read_file(geojson_filepath)
     bounds = df.geometry.total_bounds
-
-    fig, ax = plt.subplots(figsize = (_XSIZE,_YSIZE))
-
+    fig, ax = plt.subplots(figsize = (xsize, ysize))
+    
     df.to_crs(epsg=4326).plot(column='SIC', ax=ax, edgecolor='grey', linewidth=0.5, cmap = 'coolwarm')
-
     df.to_crs(epsg=4326).plot(column='land', ax=ax, edgecolor='brown', linewidth=0.5, cmap = 'copper')
 
-    plt.savefig('amsr_southern_SDA.vessel.geojson.png', dpi=180)
+    plt.savefig(os.path.join(geojson_filepath, '.png'), dpi=dpi)
+
+    return
 
 
 def main():
@@ -33,28 +52,21 @@ def main():
     script entry point
     """
     
-    parser = argparse.ArgumentParser(description='perform action with simple-action-pipeline by supplying a pipeline directory')
-    parser.add_argument("action", help="Action for the pipeline to perform. \
-                        options are 'build', 'status', 'execute', 'reset', 'halt'.", \
-                            type=str, choices=['build', 'status', 'execute', 'reset', 'halt'])
-    parser.add_argument("pipeline_directory", help="Pipeline directory to use", nargs="*", default="./")
-    parser.add_argument("-d", "--directory", help="Pipeline directory", action="store", dest='pipedir')
-    parser.add_argument("-f", "--force-rebuild", help="Force building the pipeline that is already built.", action="store_true", dest='rebuild', default=False)
-    parser.add_argument("-s", "--short", help="Output shortened information where supported", action="store_true", dest='short', default=False)
+    parser = argparse.ArgumentParser(description='Generate PNG summary image from Meshiphi GEOJSON output')
+    parser.add_argument("-w", "--widthscale", help="WidthScale in inches", action="store", dest='xsize', default=_XSIZE)
+    parser.add_argument("-h", "--heightscale", help="HeightScale in inches", action="store", dest='ysize', default=_YSIZE)
+    parser.add_argument("-d", "--dpi", help="Output resolution DPI", action="store", dest='dpi', default=_DPI)
+    parser.add_argument("files", help="One or more Meshiphi GEOJSON mesh file path(s)", type=str, nargs='+')
     args = parser.parse_args()
 
 
     # Now kick off main
 
-    # What to do if no pipeline directory was supplied
-    if (not args.pipedir) and (not args.pipeline_directory):
-        logger.warning("No pipeline directory provided. Attempting to use current directory as pipeline.")
+    checked_files = check_input_filenames(args.files)
 
-    # Keyword switch takes priority over positional switch
-    if (args.pipedir):
-        args.pipeline_directory = args.pipedir
-    
-    perform(args.pipeline_directory, args.action, args.rebuild, args.short)
+    for geojson_filepath in checked_files:
+        convert_to_png(geojson_filepath, args.xsize, args.ysize, args.dpi)
+
 
 
 if __name__ == "__main__":
